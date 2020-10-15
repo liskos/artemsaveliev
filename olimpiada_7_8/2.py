2ed96b9628d56f6b6b46576e0db171bed0c9262b
Artem <artem_savelev2006@bk.ru>
2020-10-15 20:43:45 +0300

    100% неправильное задание номер 2.2)

--- olimpiada_7_8/2.py
+++ olimpiada_7_8/2.py
@@ -1,17 +1,13 @@
-a = 157
 k = 0
-while a > 0:
-    a //= 10
+for i1 in "FNS":
     k += 1
-    for i1 in "FNS":
-        a //= 10
-        for i2 in "FNS":
-            a //= 10
-            for i3 in "FNS":
-                a //= 10
-                for i4 in "FNS":
-                    a //= 10
-                    for i5 in "FNS":
-                        a //= 10
-                        i = i1 + i2 + i3 + i4 + i5
+    for i2 in "FNS":
+        k += 1
+        for i3 in "FNS":
+             k += 1
+             for i4 in "FNS":
+                k += 1
+                for i5 in "FNS":
+                    k += 1
+                    i = i1 + i2 + i3 + i4 + i5
 print(k, i)
