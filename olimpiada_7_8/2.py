429424e4258a6ddc38d334faacb1f31bfb1f2412
Artem <artem_savelev2006@bk.ru>
2020-10-15 21:01:41 +0300

    100% неправильное задание номер 2.2.1)

--- olimpiada_7_8/2.py
+++ olimpiada_7_8/2.py
@@ -1,12 +1,8 @@
 k = 0
 for i1 in "FNS":
-    k += 1
     for i2 in "FNS":
-        k += 1
         for i3 in "FNS":
-             k += 1
              for i4 in "FNS":
-                k += 1
                 for i5 in "FNS":
                     k += 1
                     i = i1 + i2 + i3 + i4 + i5
